b349ce2771192e7862f0444bed7c39666f10d613
Johan Bloemberg <github@ijohan.nl>
2016-09-14 19:09:04 +0200

    global ssl settings not supported yet.

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,8 +42,6 @@ class sites (
     log_format              => {
       cache => '$remote_addr - $upstream_cache_status [$time_local] $request $status $body_bytes_sent $http_referer $http_user_agent',
     },
-    # global ssl settings
-    ssl_ciphers             => $ssl_ciphers,
   }
   class {'nginx': }
 
